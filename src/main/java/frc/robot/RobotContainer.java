e4e1e4ef9521fbcaba5a0decb1cf5db61e31caf4
im-a-robo <nmdas05x@gmail.com>
2023-01-11 18:38:54 -0500

    added drivebase with neos, needs motor ID's and gains #5

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,7 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.thunder.LightningContainer;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,46 +12,54 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
-public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+public class RobotContainer extends LightningContainer{
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
+  @Override
+  protected void configureButtonBindings() {
+    // TODO Auto-generated method stub
+    
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  @Override
+  protected void configureSystemTests() {
+    // TODO Auto-generated method stub
+    
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+  @Override
+  protected void configureDefaultCommands() {
+    // TODO Auto-generated method stub
+    
   }
+
+  @Override
+  protected void releaseDefaultCommands() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  protected void initializeDashboardCommands() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  protected void configureAutonomousCommands() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  protected void configureFaultCodes() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  protected void configureFaultMonitors() {
+    // TODO Auto-generated method stub
+    
+  }
+
 }
