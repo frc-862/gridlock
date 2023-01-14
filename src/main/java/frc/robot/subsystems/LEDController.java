c4db2df1cf200d5d6a50590f630544c5dd2e8026
Ishan <littleishan@gmail.com>
2023-01-14 11:13:33 -0500

    [#33] cleaned up LED code a bit

--- src/main/java/frc/robot/subsystems/LEDController.java
+++ src/main/java/frc/robot/subsystems/LEDController.java
@@ -9,20 +9,22 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.XboxController;
 
-
 public class LEDController extends SubsystemBase {
+
+  int ledPort = 9;
+  int ledLength = 36;
 
   AddressableLED led;
   AddressableLEDBuffer ledBuffer;
 
   /** Creates a new LEDsController. */
   public LEDController() {
-    led = new AddressableLED(9);
+    led = new AddressableLED(ledPort);
 
     // Reuse buffer
     // Default to a length of 60, start empty output
     // Length is expensive to set, so only set it once, then just update data
-    ledBuffer = new AddressableLEDBuffer(37);
+    ledBuffer = new AddressableLEDBuffer(ledLength);
     led.setLength(ledBuffer.getLength());
 
     // Set the data
@@ -31,48 +33,48 @@ public class LEDController extends SubsystemBase {
   }
 
   public void readyCollect(){
-    for(int i=0; i<37; i ++){
+    for(int i=0; i<=ledLength; i ++){
       ledBuffer.setRGB(i, 0, 255, 0);
     }
   }
 
   public void hasGamePiece(){
-    for(int i=0; i<37; i++){
+    for(int i=0; i<=ledLength; i++){
       ledBuffer.setRGB(i, 0, 0, 255);
     }
   }
 
   public void readyScore(){
-    for(int i=0; i<37; i++){
+    for(int i=0; i<=ledLength; i++){
       ledBuffer.setRGB(i, 230, 50, 200);
     }
   }
 
   public void readyDrop(){
-    for(int i=0; i<37; i++){
+    for(int i=0; i<=ledLength; i++){
       ledBuffer.setRGB(i, 96, 209, 149);
     }
   }
   public void believeScored(){
-    for(int i=0; i<37; i++){
-      ledBuffer.setRGB(i, 219, 146, 37);
+    for(int i=0; i<=ledLength; i++){
+      ledBuffer.setRGB(i, 219, 146, ledLength);
     }
   }
 
   public void wantsCone(){
-    for(int i=0; i<37; i++){
+    for(int i=0; i<=ledLength; i++){
       ledBuffer.setRGB(i, 255, 230, 30);
     }
   }
   
   public void wantsCube(){
-    for(int i=0; i<37; i++){
+    for(int i=0; i<=ledLength; i++){
       ledBuffer.setRGB(i, 220, 30, 240);
     }
   }
 
   public void clearLED(){
-    for(int i=0; i<37; i++){
+    for(int i=0; i<=ledLength; i++){
       ledBuffer.setRGB(i, 225, 225, 225);
     }
   }
