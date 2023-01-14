50af2c6cba7aea7c99c8ef26aa554b225db28c99
Ishan <littleishan@gmail.com>
2023-01-14 12:07:40 -0500

    [#33] added blink code and state handling

--- src/main/java/frc/robot/subsystems/LEDController.java
+++ src/main/java/frc/robot/subsystems/LEDController.java
@@ -17,6 +17,9 @@ public class LEDController extends SubsystemBase {
   AddressableLED led;
   AddressableLEDBuffer ledBuffer;
 
+  String prevState = "none";
+  String currentState = "none";
+
   /** Creates a new LEDsController. */
   public LEDController() {
     led = new AddressableLED(ledPort);
@@ -36,56 +39,104 @@ public class LEDController extends SubsystemBase {
     for(int i=0; i<=ledLength; i ++){
       ledBuffer.setRGB(i, 0, 255, 0);
     }
+
+    currentState = "readyCollect";
   }
 
   public void hasGamePiece(){
     for(int i=0; i<=ledLength; i++){
       ledBuffer.setRGB(i, 0, 0, 255);
     }
+
+    currentState = "hasGamePiece";
   }
 
   public void readyScore(){
     for(int i=0; i<=ledLength; i++){
       ledBuffer.setRGB(i, 230, 50, 200);
     }
+
+    currentState = "readyScore";
   }
 
   public void readyDrop(){
     for(int i=0; i<=ledLength; i++){
       ledBuffer.setRGB(i, 96, 209, 149);
     }
+
+    currentState = "readyDrop";
   }
+
   public void believeScored(){
     for(int i=0; i<=ledLength; i++){
       ledBuffer.setRGB(i, 219, 146, ledLength);
     }
+
+    currentState = "believeScored";
   }
 
   public void wantsCone(){
     for(int i=0; i<=ledLength; i++){
       ledBuffer.setRGB(i, 255, 230, 30);
     }
+
+    currentState = "wantsCone";
   }
   
   public void wantsCube(){
     for(int i=0; i<=ledLength; i++){
       ledBuffer.setRGB(i, 220, 30, 240);
     }
+
+    currentState = "wantsCube";
   }
 
   public void clearLED(){
     for(int i=0; i<=ledLength; i++){
       ledBuffer.setRGB(i, 225, 225, 225);
     }
+
+    currentState = "clearLED";
+  }
+
+  public void blink(){
+    if ((System.currentTimeMillis() % 1000) < 500){
+      for(int i=0; i<=ledLength; i++){
+        ledBuffer.setRGB(i, 0, 0, 225);
+      }
+    } else {
+      for(int i=0; i<=ledLength; i++){
+        ledBuffer.setRGB(i, 225, 0, 0);
+      }
+    }
+
+    currentState = "blink";
   }
 
   public void stop(){
     led.stop();
   }
 
+  public void chooseState(){
+    //future code here to decide what state to use - for now just blink
+    blink();
+  }
+
   // This method will be called once per scheduler run
   @Override
   public void periodic() {
+
+    chooseState();
+
+    //for states like blink, we need to keep refreshing it. otherwise, we should only set it once
+    if (prevState.equals(currentState)){
+      if (currentState.equals("blink")){ 
+        led.setData(ledBuffer);
+      }
+    } else {
       led.setData(ledBuffer);
+      prevState = currentState;
+    }
+
   }
 }
