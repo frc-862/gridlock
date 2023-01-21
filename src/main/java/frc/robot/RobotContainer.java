bfa4bd5a377af38809b932b613901bf00578ad03
TurtleMaster29 <73084971+TurtleMaster29@users.noreply.github.com>
2023-01-21 11:11:59 -0500

    [#33] Added support for blinking and orangeAndBlue color

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,8 +4,14 @@
 
 package frc.robot;
 
+import java.util.Map;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.LEDController;
 import frc.thunder.LightningContainer;
@@ -26,15 +32,18 @@ public class RobotContainer extends LightningContainer {
     led = new LEDController();
 
     if(led != null) {
-      tab.add(new InstantCommand(led::readyCollect, led));
-      tab.add(new InstantCommand(led::hasGamePiece, led));
-      tab.add(new InstantCommand(led::readyScore, led));
-      tab.add(new InstantCommand(led::readyDrop, led));
-      tab.add(new InstantCommand(led::believeScored, led));
-      tab.add(new InstantCommand(led::wantsCone, led));
-      tab.add(new InstantCommand(led::wantsCube, led));
-      tab.add(new InstantCommand(led::fullWhite, led));
-      tab.add(new InstantCommand(led::blink, led));
+      tab.add("readyCollect", new InstantCommand(led::readyCollect, led));
+      tab.add("hasGamePiece", new InstantCommand(led::hasGamePiece, led));
+      tab.add("readyScore", new InstantCommand(led::readyScore, led));
+      tab.add("readyDrop", new InstantCommand(led::readyDrop, led));
+      tab.add("believeScored", new InstantCommand(led::believeScored, led));
+      tab.add("wantsCone", new InstantCommand(led::wantsCone, led));
+      tab.add("wantsCube", new InstantCommand(led::wantsCube, led));
+      tab.add("fullWhite", new InstantCommand(led::fullWhite, led));
+      tab.add("blink", new InstantCommand(led::blink, led));
+      tab.add("stop", new InstantCommand(led::stop, led));
+      tab.add("start", new InstantCommand(led::start, led));
+      tab.add("orangeAndBlue", new InstantCommand(led::orangeAndBlue, led));
     }
   }
 
--- src/main/java/frc/robot/subsystems/LEDController.java
+++ src/main/java/frc/robot/subsystems/LEDController.java
@@ -4,75 +4,103 @@
 
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.util.Currency;
+
+// import java.util.Map;
+// import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 public class LEDController extends SubsystemBase {
 
   //initialize variables
   int ledPort = 9;
-  int ledLength = 36;
+  final int ledLength = 37;
+  int waitTime = 10;
+  long currentTime = System.currentTimeMillis();
+  long nextEventTime = System.currentTimeMillis();
+  long timeCounter;
+
 
   AddressableLED led = new AddressableLED(ledPort);
   AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(ledLength);
 
   String prevState = "none";
   String currentState = "none";
+  String currentSubstate = "none";
+
+  // private GenericEntry redControl;
+  // private GenericEntry greenControl;
+  // private GenericEntry blueControl;
+
 
   public LEDController() {
     led.setLength(ledBuffer.getLength());
 
     // Set the data
     led.setData(ledBuffer);
-    led.start();
+    start();
+
+    orangeAndBlue();
   }
 
   public void readyCollect(){
     //green
     setFullStripColor(0, 255, 0, 0.75f);
     currentState = "readyCollect";
+    led.setData(ledBuffer);
   }
 
   public void hasGamePiece(){
     //blue
     setFullStripColor(0, 0, 255, 0.75f);
     currentState = "hasGamePiece";
+    led.setData(ledBuffer);
   }
 
   public void readyScore(){
     //pink
     setFullStripColor(230, 50, 200, 0.75f);
     currentState = "readyScore";
+    led.setData(ledBuffer);
   }
 
   public void readyDrop(){
     //cyan
     setFullStripColor(96, 209, 149, 0.75f);
     currentState = "readyDrop";
+    led.setData(ledBuffer);
   }
 
   public void believeScored(){
     //red
     setFullStripColor(255, 0, 0, 0.75f);
     currentState = "believeScored";
+    led.setData(ledBuffer);
   }
 
   public void wantsCone(){
     //yellow
-    setFullStripColor(255, 230, 30, 0.75f);
+    setFullStripColor(255, 230, 20, 0.75f);
     currentState = "wantsCone";
+    led.setData(ledBuffer);
   }
   
   public void wantsCube(){
     //purple
     setFullStripColor(220, 30, 240, 0.75f);
     currentState = "wantsCube";
+    led.setData(ledBuffer);
   }
 
   public void fullWhite(){
     //white
     setFullStripColor(255, 255, 255, 0.75f);
     currentState = "fullWhite";
+    led.setData(ledBuffer);
   }
 
   public void blink(){
@@ -84,11 +112,49 @@ public class LEDController extends SubsystemBase {
     }
 
     currentState = "blink";
+    currentSubstate = "flash";
+    led.setData(ledBuffer);
+  }
+
+  public void orangeAndBlue(){
+    //flashes between blue and orange
+    if ((System.currentTimeMillis() % 1000) < 500){
+      for(int i = 0; i < ledLength; i += 2){
+        ledBuffer.setRGB(i, (int) (255 * .75), 
+                      (int) (125 * .75), 
+                      (int) (15 * .75));
+      }
+      for(int i = 1; i < ledLength; i += 2){
+        ledBuffer.setRGB(i, (int) (15 * .75), 
+                      (int) (70 * .75), 
+                      (int) (255 * .75));
+      }
+    } else {
+      for(int i = 1; i < ledLength; i += 2){
+        ledBuffer.setRGB(i, (int) (255 * .75), 
+                      (int) (125 * .75), 
+                      (int) (15 * .75));
+      }
+      for(int i = 0; i < ledLength; i += 2){
+        ledBuffer.setRGB(i, (int) (15 * .75), 
+                      (int) (70 * .75), 
+                      (int) (255 * .75));
+      }
+    }
+
+    currentState = "orangeAndBlue";
+    currentSubstate = "flash";
+    led.setData(ledBuffer);
   }
 
   //turn off LEDs
   public void stop(){
     led.stop();
+  }
+
+  //turn on LEDs
+  public void start(){
+    led.start();
   }
 
   // future code here to decide what state to use - for now just use shuffleboard
@@ -98,17 +164,19 @@ public class LEDController extends SubsystemBase {
 
   @Override
   public void periodic() {
-
-    //chooseState();
-
-    //for states like blink, we need to keep refreshing it. otherwise, we should only set it once
-    if (prevState.equals(currentState)){
-      if (currentState.equals("blink")){ 
-        led.setData(ledBuffer);
+    prevState = currentState;
+    currentTime = System.currentTimeMillis();
+    
+    //  compare current time to the next event time to take action and set next event
+    if (nextEventTime-currentTime <= 0){
+      if (currentState == "blink"){
+        blink();
       }
-    } else {
+      if (currentState == "orangeAndBlue"){
+        orangeAndBlue();
+      }
       led.setData(ledBuffer);
-      prevState = currentState;
+      nextEventTime = currentTime + 100;
     }
   }
 
@@ -121,8 +189,10 @@ public class LEDController extends SubsystemBase {
    * @param brightness decimal value for brightness where 1.0 is full brightness, 0.5 is half, etc.
    */
   private void setFullStripColor (int r, int g, int b, float brightness){
-    for(int i=0; i<=ledLength; i++){
-      ledBuffer.setRGB(i, (int) (r * brightness), (int) (g * brightness), (int) (b * brightness));
+    for(int i = 0; i < ledLength; i++){
+      ledBuffer.setRGB(i, (int) (r * brightness), 
+                      (int) (g * brightness), 
+                      (int) (b * brightness));
     } 
   }
 }
