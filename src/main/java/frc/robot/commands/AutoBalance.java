4e4b9bab5702102067b68bb0c50131dede2e7e44
Jeffthedoor <jusnoord@gmail.com>
2023-01-28 09:18:41 -0500

    [#11] solved more merge conflicts

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
--- src/main/java/frc/robot/commands/AutoBalanceBangBang.java
+++ src/main/java/frc/robot/commands/AutoBalanceBangBang.java
@@ -9,38 +9,40 @@ public class AutoBalanceBangBang extends CommandBase {
     private Drivetrain drivetrain;
     private double lastAngle;
 
-    addRequirements(drivetrain);
-  }
-
-  
-  @Override
-  public void initialize() {
-  }
-
-  
-  @Override
-  public void execute() {
-    double previousAngle = drivetrain.getPitch2d().getDegrees();
-                        
-    // TODO: tune the value of 0.05 to probably something a lot smaller
-    if (Math.abs(drivetrain.getPitch2d().getDegrees() - previousAngle) > 0.05 && drivetrain.getPitch2d().getDegrees() > Constants.OPTIMAL_ROLL) { 
-      drivetrain.setChassisSpeeds(new ChassisSpeeds(drivetrain.percentOutputToMetersPerSecond(1), 
-                                                    drivetrain.percentOutputToMetersPerSecond(0), 
-                                                    drivetrain.percentOutputToMetersPerSecond(0)));
-    } else if (Math.abs(drivetrain.getPitch2d().getDegrees() - previousAngle) > 0.05 && drivetrain.getPitch2d().getDegrees() < -Constants.OPTIMAL_ROLL) {
-      drivetrain.setChassisSpeeds(new ChassisSpeeds(drivetrain.percentOutputToMetersPerSecond(-1), 
-                                                    drivetrain.percentOutputToMetersPerSecond(0), 
-                                                    drivetrain.percentOutputToMetersPerSecond(0)));
-    } else {
-      drivetrain.stop();
-    }
-    // this measures the change in angle and if the robot is actually on a slope
-    // if it is, it will move forward until it is level and then it will move back
+    // add gyroscope
+    //TODO: initialize tanktrain
     
-    previousAngle = drivetrain.getPitch2d().getDegrees();
-	}
+    public AutoBalanceBangBang(Drivetrain drivetrain) {
+        this.drivetrain = drivetrain;
+
+        addRequirements(drivetrain);
+    }
+
+    
+    @Override
+    public void initialize() {
+        lastAngle = drivetrain.getPitch2d().getDegrees();
+    }
+
+    
+    @Override
+    public void execute() {                     
+        // TODO: tune the value of 0.05 to probably something a lot smaller
+        if (drivetrain.getPitch2d().getDegrees() - lastAngle > 0.05 && drivetrain.getPitch2d().getDegrees() > DrivetrainConstants.OPTIMAL_PITCH) { 
+            drivetrain.setChassisSpeeds(new ChassisSpeeds(drivetrain.percentOutputToMetersPerSecond(5), 
+                                                drivetrain.percentOutputToMetersPerSecond(0), 
+                                                drivetrain.percentOutputToMetersPerSecond(0)));
+        } else if (drivetrain.getPitch2d().getDegrees() - lastAngle > 0.05 && drivetrain.getPitch2d().getDegrees() < DrivetrainConstants.OPTIMAL_PITCH) {
+            drivetrain.setChassisSpeeds(new ChassisSpeeds(drivetrain.percentOutputToMetersPerSecond(-5), 
+                                                drivetrain.percentOutputToMetersPerSecond(0), 
+                                                drivetrain.percentOutputToMetersPerSecond(0)));
+        } 
+        /*
+        * this measures the change in angle and if the robot is actually on a slope
+        * if it is, it will move forward until it is level and then it will move back
+        */
         
-        lastAngle = drivetrain.getPitch().getDegrees();
+        lastAngle = drivetrain.getPitch2d().getDegrees();
 	}
                 
 
