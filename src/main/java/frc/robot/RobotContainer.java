c688e06ed999b82612479b6dcbf003eb166b3ba0
TurtleMaster29 <73084971+TurtleMaster29@users.noreply.github.com>
2023-02-08 18:43:12 -0500

    [80] made functional

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -39,10 +39,10 @@ public class RobotContainer extends LightningContainer {
             new JoystickFilter(XboxControllerConstants.DEADBAND, XboxControllerConstants.MIN_POWER,
                     XboxControllerConstants.MAX_POWER, Mode.CUBED);
 
-    private static final AutonomousCommandFactory autoFactory =
-            new AutonomousCommandFactory(drivetrain::getPose, drivetrain::resetOdometry,
-                    drivetrain.getDriveKinematics(), DrivetrainConstants.DRIVE_PID_CONSTANTS,
-                    DrivetrainConstants.THETA_PID_CONSTANTS, drivetrain::setStates, drivetrain);
+    // private static final AutonomousCommandFactory autoFactory =
+    //         new AutonomousCommandFactory(drivetrain::getPose, drivetrain::resetOdometry,
+    //                 drivetrain.getDriveKinematics(), DrivetrainConstants.DRIVE_PID_CONSTANTS,
+    //                 DrivetrainConstants.THETA_PID_CONSTANTS, drivetrain::setStates, drivetrain);
 
     // Configure the button bindings
     @Override
@@ -108,4 +108,10 @@ public class RobotContainer extends LightningContainer {
 
     @Override
     protected void configureFaultMonitors() {}
+
+    @Override
+    protected AutonomousCommandFactory getCommandFactory() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
