bef471d0f9b0852f7c4de311fbec3d2099f2031a
Jeffthedoor <jusnoord@gmail.com>
2023-02-16 21:19:40 -0500

    [#166] made servo subsystem

---
+++ src/main/java/frc/robot/subsystems/ServoTurn.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.RobotMap;
+
+public class ServoTurn extends SubsystemBase {
+    Servo servo = new Servo(RobotMap.PWM.SERVO);
+
+    public ServoTurn() {}
+
+    /**
+     * Turns the servo to the specified position
+     * @param position the position to turn the servo to, between 0 and 1
+     */
+    public void turnServo(double position) {
+        servo.set(position);
+    }
+}
