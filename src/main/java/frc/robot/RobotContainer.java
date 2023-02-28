07e95f606f43e4099380dafbfcec422224e3d4d8
MattD8957 <Dowlmj8957@gmail.com>
2023-02-28 18:28:04 -0500

    [#9] removed lift

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -43,9 +43,9 @@ public class RobotContainer extends LightningContainer {
 
     // Creating our main subsystems
     private static final Drivetrain drivetrain = new Drivetrain(frontLimelight, backLimelight);
-    private static final Arm arm = new Arm();
-    private static final Wrist wrist = new Wrist();
-    private static final Elevator elevator = new Elevator();
+    // private static final Arm arm = new Arm();
+    // private static final Wrist wrist = new Wrist();
+    // private static final Elevator elevator = new Elevator();
     private static final ServoTurn servoturn = new ServoTurn();
     // private static final Lift lift = new Lift(elevator, wrist, arm);
     private static final Collector collector = new Collector();
