d6834b43a1a73a060a11e93a98e040efa1203042
Fritz Geib <fritz@standland.org>
2023-02-27 22:49:16 -0500

    [#9] copied code from tonight

--- src/main/java/frc/robot/commands/AutoAlign.java
+++ src/main/java/frc/robot/commands/AutoAlign.java
@@ -1,61 +1,67 @@
+
 package frc.robot.commands;
 
-    import edu.wpi.first.math.kinematics.ChassisSpeeds;
-    import edu.wpi.first.wpilibj2.command.CommandBase;
-    import frc.robot.subsystems.Drivetrain;
-    import frc.robot.subsystems.Vision;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Vision;
 import frc.thunder.shuffleboard.LightningShuffleboard;
 
-    public class AutoAlign extends CommandBase {
-        private Drivetrain drivetrain;
-        private Vision vision;
+public class AutoAlign extends CommandBase {
+    private Drivetrain drivetrain;
+    private Vision vision;
+    private int pipeline = 0;
+    private double offset = -4.5;
+    private PIDController controller = new PIDController(0.002, 0, 0);
 
-    /** Creates a new AutoAlign. */ 
-    public AutoAlign(Drivetrain drivetrain, Vision vision) {
+    public AutoAlign(Drivetrain drivetrain, Vision vision, int pipeline) {
         // Use addRequirements() here to declare subsystem dependencies.
         this.drivetrain = drivetrain;
         this.vision = vision;
-        addRequirements(drivetrain);
+        this.pipeline = pipeline;
+
+        addRequirements(drivetrain, vision);
     }
 
     @Override
-    public void initialize() {}
+    public void initialize() {
+        vision.setPipelineNum(pipeline);
+        controller.setSetpoint(offset);
+        controller.setTolerance(3d);
+    }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        LightningShuffleboard.setDouble("AutoAlign", "Degree offset from tar", vision.autoAlign());
-        LightningShuffleboard.setDouble("AutoAlign", "Distance from tar", vision.calculateRetroReflectiveDistance());
+        if (!isOnTarget(vision.getHorizontalOffset()) && vision.getHasVision()) {
+            drivetrain.drive(ChassisSpeeds.fromFieldRelativeSpeeds(
 
-        //Gets distance to travel along the y axis
-        double distance = vision.calculateRetroReflectiveDistance() * Math.sin(vision.autoAlign());
-        LightningShuffleboard.setDouble("AutoAlign", "Distance on y", distance);
-        // TODO: Tune this value IT is the scalar for speed 
-        distance *= 0.25; 
-        // THis is kinda dumb
-
-        drivetrain.setChassisSpeeds(new ChassisSpeeds(0d, distance, 0d));
-
-        if (isFinished()){
-            end(false);
+                    drivetrain.percentOutputToMetersPerSecond(
+                            controller.calculate(vision.getHorizontalOffset())),
+                    drivetrain.percentOutputToMetersPerSecond(0d),
+                    drivetrain.percentOutputToRadiansPerSecond(0d), drivetrain.getYaw2d()));
+        } else {
+            drivetrain.stop();
         }
+    }
+
+    public boolean isOnTarget(double currentAngle) {
+        currentAngle -= 4.5;
+        // Should put consideration into how accurate we want to be later on.
+        return Math.abs(currentAngle) < 3;
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
         drivetrain.stop();
+        vision.setPipelineNum(0);
     }
 
     // Returns true when the command should end.
     @Override
-    public boolean isFinished() {
-        double horizAngleToTarget = vision.autoAlign();
-        boolean isOnTarget = vision.isOnTarget(horizAngleToTarget);
-        if (isOnTarget){
-            return true;
-        } else {
-            return false;
-        }
+    public boolean isFinished() {;
     }
+
 }
