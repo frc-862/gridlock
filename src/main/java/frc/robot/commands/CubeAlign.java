ab8bf20ca2928bc15f3cbcb76e03230c6a0599b8
MattD8957 <Dowlmj8957@gmail.com>
2023-04-13 18:27:51 -0400

    [#289] Added stuff

--- src/main/java/frc/robot/commands/CubeAlign.java
+++ src/main/java/frc/robot/commands/CubeAlign.java
@@ -8,13 +8,16 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.AutoAlignConstants;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.LimelightFront;
+import frc.thunder.limelightlib.LimelightHelpers;
+import frc.thunder.shuffleboard.LightningShuffleboard;
 import frc.thunder.swervelib.DriveController;
 
 public class CubeAlign extends CommandBase {
     
     private Drivetrain drivetrain;
     private LimelightFront limelightFront;
-    private double horizOffset;
+    private double horizOffsetLength;
+    private double horizOffsetDegrees;
     private Pose2d translatedPose = new Pose2d();
     private double XOffset = 0;
     private double YOffset = 0;
@@ -35,15 +38,21 @@ public class CubeAlign extends CommandBase {
     @Override
     public void execute() {
         if (limelightFront.hasVision() && limelightFront.getPipelineNum() == 3){
-            horizOffset = limelightFront.getHorizontalOffset();
-            horizOffset *= AutoAlignConstants.HORIZONTAL_MULTIPLIER;
-            XOffset = horizOffset / Math.sin(0d/*GET FROM VISION*/);
-            YOffset = horizOffset / Math.tan(0d/*GET FROM VISION*/);
+            horizOffsetLength = limelightFront.getHorizontalOffset();
+            horizOffsetLength *= AutoAlignConstants.HORIZONTAL_MULTIPLIER;
+            horizOffsetDegrees = LimelightHelpers.getLimelightNTDouble("Retro", "txp");
+            XOffset = horizOffsetLength / Math.sin(horizOffsetDegrees);
+            YOffset = horizOffsetLength / Math.tan(horizOffsetDegrees);
         }
 
         translatedPose = new Pose2d(new Translation2d(drivetrain.getPose().getX() - XOffset, drivetrain.getPose().getY() - YOffset), drivetrain.getPose().getRotation());
 
         drivetrain.resetOdometry(translatedPose);
+
+        LightningShuffleboard.setDouble("Cube-Align", "X offset", XOffset);
+        LightningShuffleboard.setDouble("Cube-Align", "Y offset", YOffset);
+        LightningShuffleboard.setDouble("Cube-Align", "horizontal distance offset", horizOffsetLength);
+        LightningShuffleboard.setDouble("Cube-Align", "horizontal angle offset", horizOffsetLength);
     }
 
     @Override
